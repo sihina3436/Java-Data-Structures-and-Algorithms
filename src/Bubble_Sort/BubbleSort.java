ca5ab60c4fe25ef12264d30a5cdc5ca5336f4836
sihina3436 <nimnadasihina@gmail.com>
2025-09-06 01:51:12 +0530

    Impliment bubble sort algorithm

---
+++ src/Bubble_Sort/BubbleSort.java
@@ -0,0 +1,48 @@
+package Bubble_Sort;
+
+public class BubbleSort {
+
+    static void BubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+
+    static void PrintArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Before Sorting:");
+        PrintArray(arr);
+
+        BubbleSort(arr);
+
+        System.out.println("After Sorting:");
+        PrintArray(arr);
+    }
+}
